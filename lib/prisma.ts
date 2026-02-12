c969d930c77cc1fb4edb5243ad8aad8972ad450b
abdelhamid11111111 <abdelhamidgoulahssen@gmail.com>
2026-02-12 16:25:00 +0100

    commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../app/generated/prisma";
+import { PrismaClient } from "@prisma/client";  // Use default import
 
 const prismaClientSingleton = () => {
   return new PrismaClient({
